423c92813300c37ad41f3456ddd80c8888bf4fee
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2024-12-04 16:39:32 +0100

    Fin paso 6

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -2,15 +2,19 @@
 
 use App\Models\Course;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
 
-it('shows courses overview', function () {
+it('shows courses overview', function (){
     // Arrange
-    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A']);
-    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B']);
-    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C']);
+    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A',
+        'released_at' => Carbon::now()]);
+    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B',
+        'released_at' => Carbon::now()]);
+    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C',
+        'released_at' => Carbon::now()]);
 
     // Act
     get(route('home'))
@@ -21,23 +25,36 @@ it('shows courses overview', function () {
             'Description Course B',
             'Course C',
             'Description Course C',
+    ]);
+});
+
+it('shows only released courses', function (){
+    // Arrange
+    Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B']);
+
+    // Act
+    get(route('home'))
+        ->assertSeeText([
+        'Course A',
+    ])
+        ->assertDontSee([
+        'Course B',
+    ]);
+
+    // Assert
+});
+
+it('shows courses by release date', function (){
+    // Arrange
+    Course::factory()->create(['title' => 'Course A', 'released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B', 'released_at' => Carbon::now()]);
+
+    // Act
+    get(route('home'))
+        ->assertSeeText([
+            'Course A',
+            'Course B',
         ]);
-});
-
-it('shows only released courses', function () {
-    // Arrange
-
-    // Act
-
     // Assert
-
-});
-
-it('shows courses by release date', function () {
-    // Arrange
-
-    // Act
-
-    // Assert
-
 });
